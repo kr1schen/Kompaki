3b281fd040af250db6bd48f5f415a44dc9f82753
kr1schen <107362001+kr1schen@users.noreply.github.com>
2024-06-23 16:08:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -611,4 +611,4 @@ def KPI():
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000, debug=True)
